97121e650aa3ba7f4a6c2e3b02c44f85b2ffdbba
Vaibhav Gurav <151390343+VaibhavGurav07@users.noreply.github.com>
2025-01-21 11:39:43 +0000

    update

--- Guessing_Number.py
+++ Guessing_Number.py
@@ -10,17 +10,18 @@ No_Guess = 7
 
 Guess_num = 0
 
-while No_Guess >=Guess_num :
+while No_Guess >Guess_num :
     
-    Guess_num = +1
+    Guess_num += 1
     Number_input = int (input("Enter the Your mind numbers :- "))
 
     if Number_input == Number_to_guess:
         print("congratulations..!!!!!!!  ")
         print("Your Correct Guess Number ")
         break
-    elif No_Guess <= Guess_num and Number_input != Number_to_guess:
-        print("Your Look The Game Sorry try Again....!!!!!")
+    elif Guess_num >= No_Guess and Number_input != Number_to_guess:
+        print("Your  The Game Sorry try Again....!!!!!")
+        
     elif Number_input <Number_to_guess:
         print("Your Number is to low")
     elif Number_to_guess < Number_input:
